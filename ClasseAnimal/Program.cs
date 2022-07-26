c9e8d7ac5eff35d753799b940790bc9eaa64bd93
thaina-barbosa <thaina.mbarbosaa@gmail.com>
2022-07-25 22:48:52 -0300

    Adicionando Projeto

---
+++ ClasseAnimal/Program.cs
@@ -0,0 +1,23 @@
+﻿using Animais.Src;
+using System;
+
+namespace Cachorro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             ListaAnimais animal1  = new ("Cachorro","Thor", "Sptiz","Pequeno","Branco");
+             ListaAnimais animal2  = new ("Gato","Babi", "Siamês", "Pequeno","Marrom");
+             ListaAnimais animal3  = new ("Coelho","Fred", "Angorâ", "Pequeno","Creme");
+             ListaAnimais animal4  = new ("Leão","Mufasa", "leão-americano", "Médio","Amarelo");
+            
+
+            Console.WriteLine(animal1.ToString());
+            Console.WriteLine(animal2.ToString());
+            Console.WriteLine(animal3.ToString());
+            Console.WriteLine(animal4.ToString());
+
+        }
+    }
+}
---
+++ ClasseAnimal/Src/ListaAnimais.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animais.Src
+{
+    public class ListaAnimais
+    {
+
+        public string Animal { get; set; }
+        public string  Nome { get; set; }
+        public string Raca { get; set; }
+        public string Porte { get; set; }
+        public string Cor { get; set; }
+
+
+        public ListaAnimais(string animal, string nome, string raca, string porte, string cor)
+        {
+            Animal = animal;
+            Nome = nome;
+            Raca = raca;
+            Porte = porte;
+            Cor = cor;
+            
+        }
+
+        public override string ToString()
+        {
+            return $" Animal:{Animal} \n"+
+                   $" Nome:{Nome} \n" +
+                   $" Raça:{Raca}  \n" +
+                   $" Porte:{Porte} \n"+
+                   $" Cor:{Cor} \n";
+
+        }
+        
+            
+        
+            
+
+    
+    }
+}
